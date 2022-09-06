67507252a7edc07c88cd70da3f1aa2192c4193ce
Lucas Orsi <lorsi@skyloom.co>
2022-09-06 02:18:07 -0300

    Add scripts

--- scripts/build_uboot.sh
+++ scripts/build_uboot.sh
@@ -1,1 +1,3 @@
 make libretech_all_h5_cc_h5_defconfig;
+make
+# https://linux-sunxi.org/Bootable_SD_card#Bootloader
---
+++ scripts/prepare_sdcard.sh
@@ -0,0 +1,18 @@
+readonly BOOTLOADER_PATH=./u-boot/u-boot-sunxi-with-spl.bin 
+# SD Card via MMC (micro sd)
+export card=/dev/mmcblk0
+export p=p
+# Cleaning
+dd if=/dev/zero of=${card} bs=1M count=1
+# Bootloader
+dd if=$BOOTLOADER_PATH of=${card} bs=1024 seek=8
+# Separate boot partition
+blockdev --rereadpt ${card}
+cat <<EOT | sfdisk ${card}
+1M,16M,c
+,,L
+EOT
+# Other partitions
+# mkfs.vfat ${card}${p}1
+# mkfs.ext4 ${card}${p}2
+# cardroot=${card}${p}2
